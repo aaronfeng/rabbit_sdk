fd04a60c16f93b91ac3bb26ec850e63bf723ddd9
Matthias Radestock <matthias@matthias-netbook.(none)>
2009-07-30 16:15:37 +0100

    cosmetic

--- src/Unicast.cs
+++ src/Unicast.cs
@@ -67,28 +67,28 @@ namespace RabbitMQ.Client.MessagePatterns.Unicast {
             get { return m_properties; }
             set { m_properties = value; }
         }
-        public byte[]           Body       {
+        public byte[] Body {
             get { return m_body; }
             set { m_body = value; }
         }
-        public string           RoutingKey {
+        public string RoutingKey {
             get { return m_routingKey; }
             set { m_routingKey = value; }
         }
 
-        public Address From            {
+        public Address From {
             get { return Properties.UserId; }
             set { Properties.UserId = value; }
         }
-        public Address To              {
+        public Address To {
             get { return RoutingKey; }
             set { RoutingKey = value; }
         }
-        public Address ReplyTo         {
+        public Address ReplyTo {
             get { return Properties.ReplyTo; }
             set { Properties.ReplyTo = value; }
         }
-        public MessageId MessageId     {
+        public MessageId MessageId {
             get { return Properties.MessageId; }
             set { Properties.MessageId = value; }
         }
@@ -162,23 +162,23 @@ namespace RabbitMQ.Client.MessagePatterns.Unicast {
 
         public event MessageEventHandler Sent;
 
-        public Address Identity      {
+        public Address Identity {
             get { return m_identity; }
             set { m_identity = value; }
         }
-        public Name    ExchangeName  {
+        public Name ExchangeName {
             get { return m_exchangeName; }
             set { m_exchangeName = value; }
         }
-        public string  ExchangeType  {
+        public string ExchangeType {
             get { return m_exchangeType; }
             set { m_exchangeType = value; }
         }
-        public Name    QueueName     {
+        public Name QueueName {
             get { return ("".Equals(m_queueName) ? Identity : m_queueName); }
             set { m_queueName = value; }
         }
-        public ushort  PrefetchLimit {
+        public ushort PrefetchLimit {
             get { return m_prefetchLimit; }
             set { m_prefetchLimit = value; }
         }
@@ -188,17 +188,17 @@ namespace RabbitMQ.Client.MessagePatterns.Unicast {
             set { m_createSubscription = value; }
         }
 
-        public IConnection  Connection       {
+        public IConnection Connection {
             get { return m_connection; }
         }
-        public IModel       SendingChannel   {
+        public IModel SendingChannel {
             get { return m_sendingChannel; }
         }
-        public IModel       ReceivingChannel {
+        public IModel ReceivingChannel {
             get { return m_receivingChannel; }
         }
 
-        public Subscription Subscription     {
+        public Subscription Subscription {
             get { return m_subscription; }
         }
 
@@ -231,12 +231,12 @@ namespace RabbitMQ.Client.MessagePatterns.Unicast {
                                      m.Identity));
         }
 
-        public MessageId        NextId() {
+        public MessageId NextId() {
             return System.String.Format("{0:x8}{1:x8}",
                                         m_msgIdPrefix, m_msgIdSuffix++);
         }
 
-        public void             Send(IMessage m) {
+        public void Send(IMessage m) {
             SendingChannel.BasicPublish(ExchangeName, m.RoutingKey,
                                         m.Properties, m.Body);
             //TODO: if/when SendingChannel supports 'sent'
@@ -250,7 +250,7 @@ namespace RabbitMQ.Client.MessagePatterns.Unicast {
             return (e == null) ? null : new ReceivedMessage(e);
         }
 
-        public void             Ack(IReceivedMessage m) {
+        public void Ack(IReceivedMessage m) {
             Subscription.Ack(((ReceivedMessage)m).Delivery);
         }
 
